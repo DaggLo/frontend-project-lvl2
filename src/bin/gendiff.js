5394bbdd4ede2151e6bc5357588bd60ff02a4132
Evgeny Degtev <e.a.degtev@gmail.com>
2020-10-15 14:58:53 +0300

    feat: Add -f flag into description.
    Finished step 2 of the project.

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,6 +5,13 @@ import { version } from '../../package.json';
 
 program
   .version(version)
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  /* .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig, cmdObj) => {
+    console.log(firstConfig);
+    console.log(secondConfig);
+    console.log(cmdObj.format ? cmdObj.format : 'no options passed');
+  }) */;
 
 program.parse(process.argv);
