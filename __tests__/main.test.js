28582a4c6339cae20f3b10eb61fc93d977c72c16
Evgeny Degtev <e.a.degtev@gmail.com>
2020-10-26 22:21:47 +0300

    test: Refactor main module tests.

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,26 +1,66 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { generateDiffString } from '../src/main';
+import gendiff, { generateDiff, toFormatedString } from '../src/main';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
 
-test('should work', () => {
+describe('Test main functionality.', () => {
   const data1 = JSON.parse(readFile('flat1.json'));
   const data2 = JSON.parse(readFile('flat2.json'));
 
-  const expected = [
-    '{',
-    '  - follow: false',
-    '    host: hexlet.io',
-    '  - proxy: 123.234.53.22',
-    '  - timeout: 50',
-    '  + timeout: 20',
-    '  + verbose: true',
-    '}',
-  ].join('\n');
+  describe('Generating diff.', () => {
+    const expected = [
+      [' ', 'host', 'hexlet.io'],
+      ['-', 'timeout', 50],
+      ['+', 'timeout', 20],
+      ['-', 'proxy', '123.234.53.22'],
+      ['-', 'follow', false],
+      ['+', 'verbose', true],
+    ];
 
-  expect(generateDiffString(data1, data2)).toEqual(expected);
+    test('should work', () => {
+      expect(generateDiff(data1, data2)).toEqual(expected);
+    });
+  });
+
+  describe('Convert to formatted string.', () => {
+    const actual = generateDiff(data1, data2);
+    const expected = [
+      '{',
+      '  - follow: false',
+      '    host: hexlet.io',
+      '  - proxy: 123.234.53.22',
+      '  - timeout: 50',
+      '  + timeout: 20',
+      '  + verbose: true',
+      '}',
+    ].join('\n');
+
+    test('should work', () => {
+      expect(toFormatedString(actual)).toEqual(expected);
+    });
+  });
+
+  describe('Overall flow.', () => {
+    const path1 = getFixturePath('flat1.json');
+    const path2 = getFixturePath('flat2.json');
+    const actual = gendiff(path1, path2);
+    const expected = [
+      '{',
+      '  - follow: false',
+      '    host: hexlet.io',
+      '  - proxy: 123.234.53.22',
+      '  - timeout: 50',
+      '  + timeout: 20',
+      '  + verbose: true',
+      '}',
+    ].join('\n');
+
+    test('should work', () => {
+      expect(actual).toEqual(expected);
+    });
+  });
 });
