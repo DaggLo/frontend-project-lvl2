7e74e1eb864332b9f2bef878b143422161bd09f1
Evgeny Degtev <e.a.degtev@gmail.com>
2020-11-02 23:48:29 +0300

    refactor: Move getting data, diff generation and to string convertion into index.js.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-import gendiff from './src/main.js';
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
 
-export default gendiff;
+import { generateDiff, parse, toFormatedString } from './src/flatJSON.js';
+
+const getParsedData = (filePath) => {
+  const __dirname = process.cwd();
+  const actualPath = path.resolve(__dirname, filePath);
+  const data = fs.readFileSync(actualPath, 'utf8');
+  return parse(data);
+};
+
+export default (filePath1, filePath2) => {
+  const diff = generateDiff(getParsedData(filePath1), getParsedData(filePath2));
+  return toFormatedString(diff);
+};
+
+export { getParsedData };
