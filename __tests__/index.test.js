21c1e0942455602754928cd3e4ff571b76dea9a1
Evgeny Degtev <e.a.degtev@gmail.com>
2020-11-13 18:11:25 +0300

    test: Leave only main test. Add corner cases.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,57 +1,41 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import gendiff, { getParsedData } from '../index.js';
+import gendiff from '../index.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-describe('Getting parsed data.', () => {
-  describe('Flat JSON', () => {
-    const path1 = getFixturePath('flat1.json');
-    const path2 = getFixturePath('flat2.json');
-    const actual1 = getParsedData(path1);
-    const actual2 = getParsedData(path2);
+describe('Test overall package operability.', () => {
+  describe('Standard cases.', () => {
+    test('Comparing two JSON files.', () => {
+      const path1 = getFixturePath('flat1.json');
+      const path2 = getFixturePath('flat2.json');
+      const actual = gendiff(path1, path2);
+      const expected = [
+        '{',
+        '  - follow: false',
+        '    host: hexlet.io',
+        '  - proxy: 123.234.53.22',
+        '  - timeout: 50',
+        '  + timeout: 20',
+        '  + verbose: true',
+        '}',
+      ].join('\n');
 
-    const expected1 = {
-      host: 'hexlet.io',
-      timeout: 50,
-      proxy: '123.234.53.22',
-      follow: false,
-    };
-
-    const expected2 = {
-      timeout: 20,
-      verbose: true,
-      host: 'hexlet.io',
-    };
-
-    test('should work #1', () => {
-      expect(actual1).toEqual(expected1);
-    });
-    test('should work #2', () => {
-      expect(actual2).toEqual(expected2);
+      expect(actual).toEqual(expected);
     });
   });
-});
 
-describe('Test overall module flow.', () => {
-  describe('Compare two JSON files.', () => {
-    const path1 = getFixturePath('flat1.json');
-    const path2 = getFixturePath('flat2.json');
-    const actual = gendiff(path1, path2);
-    const expected = [
-      '{',
-      '  - follow: false',
-      '    host: hexlet.io',
-      '  - proxy: 123.234.53.22',
-      '  - timeout: 50',
-      '  + timeout: 20',
-      '  + verbose: true',
-      '}',
-    ].join('\n');
+  describe('Corner cases.', () => {
+    test('Incorrect paths', () => {
+      const path1 = getFixturePath('flat1.json');
+      const path2 = getFixturePath('flat2.json');
 
-    test('should work', () => {
-      expect(actual).toEqual(expected);
+      expect(gendiff()).toBeNull();
+      expect(gendiff(path1)).toBeNull();
+      expect(gendiff(path2)).toBeNull();
+      expect(gendiff(path1, path2, 'something')).toBeNull();
+      expect(gendiff(path1, '.')).toBeNull();
     });
   });
 });
