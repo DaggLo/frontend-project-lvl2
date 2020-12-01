50a9104d73e8c1f572d25ef6c1e7545b09b97d09
Evgeny Degtev <e.a.degtev@gmail.com>
2020-12-01 16:12:04 +0300

    test: Add test of args validating.

--- __tests__/src/main.test.js
+++ __tests__/src/main.test.js
@@ -1,7 +1,9 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { generateDiff, getData, toFormatedString } from '../../src/main.js';
+import {
+  generateDiff, getData, isValidArgs, toFormatedString,
+} from '../../src/main.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '../..', '__fixtures__', filename);
@@ -36,6 +38,46 @@ describe('Getting data.', () => {
 
     expect(actual1).toEqual(expected1);
     expect(actual2).toEqual(expected2);
+  });
+});
+
+describe('Args validation.', () => {
+  const path1 = getFixturePath('flat1.json');
+  const path2 = getFixturePath('flat2.json');
+  const path3 = getFixturePath('flat1.yml');
+  const path4 = getFixturePath('flat2.yml');
+
+  describe('Valid args.', () => {
+    test('Should work.', () => {
+      const actual1 = isValidArgs(path1, path2);
+      const actual2 = isValidArgs(path3, path4);
+      const actual3 = isValidArgs(path1, path4);
+      const actual4 = isValidArgs(path2, path3);
+
+      expect(actual1).toBe(true);
+      expect(actual2).toBe(true);
+      expect(actual3).toBe(true);
+      expect(actual4).toBe(true);
+    });
+  });
+
+  describe('Not valid args.', () => {
+    test('More or less than two args.', () => {
+      expect(isValidArgs()).toBe(false);
+      expect(isValidArgs(path1)).toBe(false);
+      expect(isValidArgs(path1, path2, path3)).toBe(false);
+      expect(isValidArgs(path1, path2, path3, path4)).toBe(false);
+    });
+
+    test('Not valid paths.', () => {
+      expect(isValidArgs(3, path2)).toBe(false);
+      expect(isValidArgs(null, path3)).toBe(false);
+      expect(isValidArgs(path1, undefined)).toBe(false);
+      expect(isValidArgs('', path4)).toBe(false);
+      expect(isValidArgs(path3, 'hehehe')).toBe(false);
+      expect(isValidArgs('.', path2)).toBe(false);
+      expect(isValidArgs(path1, '..')).toBe(false);
+    });
   });
 });
 
