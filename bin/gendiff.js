be6cc4e71a010728a6da4d01d4acffc4ff79baaa
Evgeny Degtev <e.a.degtev@gmail.com>
2020-12-26 23:03:04 +0300

    feat: Add version import from package.json into the bin.

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,22 +6,28 @@
 // env-invocation.html#g_t_002dS_002f_002d_002dsplit_002dstring-usage-in-scripts
 
 import program from 'commander';
-// import * as packageConfig from '../package.json';
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import gendiff from '../index.js';
 
-// const { version } = packageConfig;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filename = path.join(__dirname, '..', 'package.json');
+const packageConfig = fs.readFileSync(__filename, 'utf8');
+const { version } = JSON.parse(packageConfig);
+
 program
   .storeOptionsAsProperties(false)
   .passCommandToAction(false);
 
 program
-  .version('0.1.0')
-  // .version(version)
+  .version(version)
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<firstConfig> <secondConfig>')
   .action((firstConfig, secondConfig, options) => {
-    // console.log(firstConfig, secondConfig, options);
     const formatterName = options.format;
     const result = gendiff(firstConfig, secondConfig, formatterName);
     console.log(result);
