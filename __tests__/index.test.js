7ccfd523c801e34619c5a76bf528ea6957d5ee48
Evgeny Degtev <e.a.degtev@gmail.com>
2020-12-27 02:33:59 +0300

    test: Rename diff fixtures.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -14,46 +14,46 @@ const yaml1 = getFixturePath('1.yaml');
 const yaml2 = getFixturePath('2.yaml');
 
 describe('Standard cases.', () => {
-  const diff = readFile('diff.txt').trimEnd();
+  const expectedStylish = readFile('expected.stylish').trimEnd();
 
   test('Comparing files of same type.', () => {
-    expect(gendiff(json1, json2)).toEqual(diff);
-    expect(gendiff(yaml1, yaml2)).toEqual(diff);
+    expect(gendiff(json1, json2)).toEqual(expectedStylish);
+    expect(gendiff(yaml1, yaml2)).toEqual(expectedStylish);
   });
 
   test('Comparing files of different types.', () => {
-    expect(gendiff(yaml1, json2)).toEqual(diff);
+    expect(gendiff(yaml1, json2)).toEqual(expectedStylish);
   });
 
   test('Testing "plain" output format.', () => {
-    const diffPlain = readFile('diff_plain.txt').trimEnd();
+    const expectedPlain = readFile('expected.plain').trimEnd();
 
-    expect(gendiff(yaml1, json2, 'plain')).toEqual(diffPlain);
+    expect(gendiff(yaml1, json2, 'plain')).toEqual(expectedPlain);
   });
 
   test('Testing "json" output format.', () => {
-    const diffJson = readFile('diff.json').trimEnd();
-    const expected = JSON.stringify(JSON.parse(diffJson));
+    const rawData = readFile('expected.json').trimEnd();
+    const expectedJson = JSON.stringify(JSON.parse(rawData));
 
-    expect(gendiff(yaml1, json2, 'json')).toEqual(expected);
+    expect(gendiff(yaml1, json2, 'json')).toEqual(expectedJson);
   });
 });
 
 describe('Corner cases.', () => {
   test('Comparing files with same contents.', () => {
-    const diffSame = readFile('diff_same.txt').trimEnd();
+    const expectedSame = readFile('expected.same').trimEnd();
 
-    expect(gendiff(json1, yaml1)).toEqual(diffSame);
+    expect(gendiff(json1, yaml1)).toEqual(expectedSame);
   });
 
   test('Comparing with empty file.', () => {
     const emptyFile1 = getFixturePath('empty.json');
     const emptyFile2 = getFixturePath('empty.yml');
 
-    const diffEmpty = readFile('diff_empty.txt').trimEnd();
+    const expectedEmpty = readFile('expected.empty').trimEnd();
 
-    expect(gendiff(emptyFile1, json1)).toEqual(diffEmpty);
-    expect(gendiff(emptyFile2, yaml1)).toEqual(diffEmpty);
+    expect(gendiff(emptyFile1, json1)).toEqual(expectedEmpty);
+    expect(gendiff(emptyFile2, yaml1)).toEqual(expectedEmpty);
   });
 });
 
@@ -67,7 +67,7 @@ describe('Processing not valid args.', () => {
 
   test('Non-existent paths.', () => {
     const wrongPath1 = getFixturePath('35.json');
-    const wrongPath2 = getFixturePath('35.json');
+    const wrongPath2 = getFixturePath('36.yaml');
 
     expect(gendiff(json1, wrongPath1)).toBeNull();
     expect(gendiff(wrongPath2, yaml2)).toBeNull();
