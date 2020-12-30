56c1c5af17695de9fb0bcfea6caa6fbd50c032b6
Evgeny Degtev <e.a.degtev@gmail.com>
2020-12-30 14:40:11 +0300

    feat: Delete custome validating.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import process from 'process';
 
 import tagData from './src/tags.js';
-import isValidArgs from './src/validater.js';
 import {
   makeDiffTree, readData, parseData, renderDiffTree,
 } from './src/main.js';
@@ -9,11 +8,6 @@ import {
 const __dirname = process.cwd();
 
 export default (filePath1, filePath2, formatterName = 'stylish') => {
-  if (!isValidArgs(filePath1, filePath2)) {
-    console.log('Please, provide correct arguments.');
-    return null;
-  }
-
   const data1 = tagData(readData(__dirname, filePath1), filePath1);
   const data2 = tagData(readData(__dirname, filePath2), filePath2);
   const diff = makeDiffTree(parseData(data1), parseData(data2));
