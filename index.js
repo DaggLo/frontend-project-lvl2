af163f28e8843bd6a369b6ffcefed48d7908bf3e
Evgeny Degtev <e.a.degtev@gmail.com>
2020-12-30 20:18:16 +0300

    refactor: Move tag making into the index.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import fs from 'fs';
+import path from 'path';
 
 import tagData from './src/tags.js';
 import { makeDiffTree, parse, format } from './src/main.js';
 
+const makeTag = (filePath) => {
+  const defaultTagName = 'Blank extension';
+  const currentExtension = path.extname(filePath);
+  return currentExtension === ''
+    ? defaultTagName
+    : currentExtension.toLowerCase();
+};
+
 const readData = (filePath) => fs.readFileSync(filePath, 'utf8');
 
 export default (filePath1, filePath2, outputFormat = 'stylish') => {
-  const data1 = tagData(readData(filePath1), filePath1);
-  const data2 = tagData(readData(filePath2), filePath2);
+  const data1 = tagData(readData(filePath1), makeTag(filePath1));
+  const data2 = tagData(readData(filePath2), makeTag(filePath2));
   const diff = makeDiffTree(parse(data1), parse(data2));
   return format(diff, outputFormat);
 };
--- src/tags.js
+++ src/tags.js
@@ -1,24 +1,10 @@
-import path from 'path';
-
-const makeTag = (arg) => {
-  const defaultTagName = 'Blank extension';
-  const currentExtension = path.extname(arg);
-  return currentExtension === ''
-    ? defaultTagName
-    : currentExtension.toLowerCase();
-};
-
-const tagData = (data, filePath) => {
-  const tag = makeTag(filePath);
-  return { tag, data };
-};
+const tagData = (data, tag) => ({ tag, data });
 
 const getTag = (taggedData) => taggedData.tag;
 
 const getData = (taggedData) => taggedData.data;
 
 export {
-  makeTag,
   getTag,
   getData,
   tagData as default,
