b6a26eb5a9ce50224a5c769c44a8b47c2db46f25
Evgeny Degtev <e.a.degtev@gmail.com>
2021-01-05 22:59:21 +0300

    refactor: Simplify making of a diff tree.

--- src/main.js
+++ src/main.js
@@ -6,38 +6,35 @@ import parsers from './parsers/index.js';
 import makeNode from './node.js';
 
 const makeDiffTree = (data1, data2, pathFromRoot = []) => {
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const diffTree = keys1.reduce(
-    (acc, key) => {
+  const keys1 = _.keys(data1);
+  const keys2 = _.keys(data2);
+  const keys = _.union(keys1, keys2);
+
+  return keys.map(
+    (key) => {
       const newPath = [...pathFromRoot, key];
+
+      if (!_.has(data1, key)) {
+        return makeNode('leaf', 'added', newPath, null, data2[key]);
+      }
+
       const { [key]: value1 } = data1;
 
-      if (!keys2.includes(key)) {
-        return [...acc, makeNode('leaf', 'deleted', newPath, value1, null)];
+      if (!_.has(data2, key)) {
+        return makeNode('leaf', 'deleted', newPath, value1, null);
       }
 
       const { [key]: value2 } = data2;
 
       if (!_.isPlainObject(value1) || !_.isPlainObject(value2)) {
         return _.isEqual(value1, value2)
-          ? [...acc, makeNode('leaf', 'unchanged', newPath, value1, value2)]
-          : [...acc, makeNode('leaf', 'changed', newPath, value1, value2)];
+          ? makeNode('leaf', 'unchanged', newPath, value1, value2)
+          : makeNode('leaf', 'changed', newPath, value1, value2);
       }
 
-      return [
-        ...acc,
-        makeNode('internal', 'unchanged', newPath, null, null, makeDiffTree(value1, value2, newPath)),
-      ];
+      return makeNode('internal', 'unchanged', newPath, null, null, makeDiffTree(value1, value2, newPath));
     },
-    [],
   );
-
-  const completedDiffTree = keys2
-    .filter((key) => !keys1.includes(key))
-    .reduce((acc, key) => [...acc, makeNode('leaf', 'added', [...pathFromRoot, key], null, data2[key])], diffTree);
-
-  return completedDiffTree;
 };
 
 const parse = (taggedData) => {
